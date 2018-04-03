305ec35a5e83f438c42292a84d891c76c5affc56
Agustin Ducca <ADucca@devesa.com>
2018-04-02 22:31:00 -0300

    building outstanding-news's section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Main Animations and JS functions
+$(document).ready(function(){
+
+	
+});
+	var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre`");
+	var f=new Date();
+	var dateShow = document.getElementById("dateField");
+	dateShow.innerHTML = f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear();
+	
